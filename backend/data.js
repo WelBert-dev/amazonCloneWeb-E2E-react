f11a47a2c45f350917da22dce18b1994c756214a
WelBert-Dev <wellison.bertelli@hotmail.com>
2022-07-20 19:33:46 -0300

    Create the backend basic server and install nodemon for auto re-running

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Nike 12 molas',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 1000,
+            countInStock: 12,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Nike Airmax',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 800,
+            countInStock: 17,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Shox',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 2500,
+            countInStock: 25,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Nike fake',
+            category: 'Shirts',
+            image: '/images/pain.png',
+            price: 20,
+            countInStock: 0,
+            brand: 'Nike Made in China',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Nike Slim Shady',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 22,
+            countInStock: 145,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+    ]
+}
+
+export default data;
