a4b26e6a2a0626353a3c8e575054ee9c15e5602a
WelBert-Dev <wellison.bertelli@hotmail.com>
2022-07-20 21:56:14 -0300

    Update de backend/server.js and change the data.js using get in api/products to backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import MainCardProduct from '../components/MainCardProduct'
-
-import data from '../data';
+import MainCardProduct from '../components/MainCardProduct';
 
 export default function HomeScreen() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const { data } = await axios.get("/api/products");
+        setProducts(data);
+      };
+      fetchData();
+    }, []);
+
     return (
         <div className="row center">
         {
-          data.products.map(product => (
+          products.map(product => (
             <MainCardProduct key={product._id} product={product} showLink={true}/>
           ))
         }         
